9632c9da3ac916e0c458ed4a48ebbd2ae5224340
GabrielWarner <gabe0412@uw.edu>
2022-08-29 11:31:10 -0700

    set up app folder to handle page changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import React, { useState } from 'react';
 import './App.css';
+import Navbar from './components/navbar';
 import Homepage from './components/pages/Home';
 import Login from './components/pages/Login';
+import Register from './components/pages/Register';
+import Dashboard from './components/pages/Dasboard';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Homepage />;
+    }
+    if (currentPage === 'Login') {
+      return <Login />;
+    }
+    if (currentPage === 'Register') {
+      return <Register />;
+    }
+    return <Dashboard />;
+  };
+
   return (
     <div className="App">
-      <Homepage/>
-      <Login/>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange}/>
+      {renderPage()}
+      
     </div>
   );
 }
--- src/components/pages/Dasboard/index.js
+++ src/components/pages/Dasboard/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function index() {
+export default function Dashboard() {
   return (
     <div>index.js</div>
   )
