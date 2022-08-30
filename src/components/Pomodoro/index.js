bc486f9998375ae6c6fb5c2e3a045d897e3e5c05
GabrielWarner <gabe0412@uw.edu>
2022-08-29 19:16:53 -0700

    added start button to timer

--- src/components/Pomodoro/index.js
+++ src/components/Pomodoro/index.js
@@ -4,36 +4,66 @@ export default function Pomodoro() {
     const [minutes, setMinutes] = useState(25)
     const [seconds, setSeconds] = useState(0)
     const [displayMessage, setDisplayMessage] = useState(false)
+    const [startButton, setStartButton] = useState(false)
+
+    const startToggle = () => {
+        // 👇️ passed function to setState
+        setStartButton(current => !current);
+      };
+
+    const setTimer = (e) => {
+        e.preventDefault()
+        console.log(e.target.value)
+        setMinutes(parseInt(e.target.value))
+        startToggle()
+    }
 
     useEffect(() => {
-        let interval = setInterval(()=>{
-            clearInterval(interval)
+        if(startButton){
 
-        if (seconds === 0) {
-            if(minutes !== 0) {
-                setSeconds(59)
-                setMinutes(minutes - 1)
-            }else{
-                let minutes = displayMessage ? 24 : 4
-                let seconds = 59;
-
-                setSeconds(seconds)
-                setMinutes(minutes)
-                setDisplayMessage(!displayMessage)
+            let interval = setInterval(()=>{
+                clearInterval(interval)
+    
+            if (seconds === 0) {
+                if(minutes !== 0) {
+                    setSeconds(59)
+                    setMinutes(minutes - 1)
+                }else{
+                    let minutes = displayMessage ? 24 : 4
+                    let seconds = 59;
+    
+    
+    
+                    setSeconds(seconds)
+                    setMinutes(minutes)
+                    setDisplayMessage(!displayMessage)
+                }
+            }else {
+                setSeconds(seconds - 1)
             }
-        }else {
-            setSeconds(seconds - 1)
+            },1000)
+        }else{
+            setSeconds(0)
+            setMinutes(25)
+            setDisplayMessage(!displayMessage)
+            return
         }
-        },1000)
-    }, [seconds])
+    }, [seconds, startButton])
     
     const timerMinutes = minutes  < 10 ? `0${minutes}` : minutes;
-    const timerSeconds = minutes  < 10 ? `0${seconds}` : minutes;
+    const timerSeconds = minutes  < 10 ? `0${seconds}` : seconds;
 
   return (
     <div className='pomodoro-container'>
+        <h1>Pomodoro Timer</h1>
+        <button onClick={()=>{startToggle()}}>Start</button>
+        <p>Or choose your own time</p>
+        <form onSubmit={(e)=>{setTimer(e)}}>
+            <input type="text" name='value'></input>
+            <button>Submit</button>
+        </form>
         <div className='message'>
-            <div>Session over. Break starts now.</div>
+           {displayMessage && <div>Session over. Break starts now.</div>}
         </div>
         <div className='timer'>{timerMinutes}:{timerSeconds}</div>
     </div>
