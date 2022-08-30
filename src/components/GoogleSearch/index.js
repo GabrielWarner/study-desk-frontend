07fb53d15a83b158fb59420a68d4c03a46ffb20b
zmzhong1 <zhongzm8e@gmail.com>
2022-08-30 16:05:41 -0700

    google search bar

---
+++ src/components/GoogleSearch/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+export default function GoogleSearch() {
+  return (
+    <div className="container">
+      <form
+        action="https://www.google.com/search"
+        method="get"
+        className="searchbar"
+      >
+        <input type="text" placeholder="search anything" name="q" />
+        <button type="submit" value="Submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
