70f4117bb7eca4f06fc925cd9ac6aa895acc980f
GabrielWarner <gabe0412@uw.edu>
2022-08-31 12:30:52 -0700

    changed references

--- src/components/pages/Dasboard/index.js
+++ src/components/pages/Dasboard/index.js
@@ -5,28 +5,28 @@ export default function Dashboard() {
   return (
     <div className='dashboard'>
       <Grid
-  templateAreas={`"nav header"
-                  "nav main"
-                  "nav main"`}
-  gridTemplateRows={'10% 1fr 30px'}
+  templateAreas={`"clock search"
+                  "side main"
+                  "side main"`}
+  gridTemplateRows={'10% 1fr 50px'}
   gridTemplateColumns={'25% 1fr'}
   h='93vh'
   gap='1'
   color='blackAlpha.700'
   fontWeight='bold'
 >
-  <GridItem pl='2' bg='orange.300' area={'header'}>
-    
+  <GridItem pl='2' bg='orange.300' area={'search'}>
+    Google Search
   </GridItem>
-  <GridItem pl='2' bg='pink.300' area={'nav'}>
-    Nav
+  <GridItem pl='2' bg='pink.300' area={'side'}>
+    side
   </GridItem>
   <GridItem pl='2' bg='green.300' area={'main'}>
     Main
   </GridItem>
-  {/* <GridItem pl='2' bg='blue.300' area={'footer'}>
-    Footer
-  </GridItem> */}
+  <GridItem pl='2' bg='blue.300' area={'clock'}>
+    Clock
+  </GridItem>
   </Grid>
     </div>
   )
