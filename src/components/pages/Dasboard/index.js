dcd1ab717b03a0e7fd1b86e829c0d9495b985263
GabrielWarner <gabe0412@uw.edu>
2022-08-31 13:33:08 -0700

    pomodoro in right spot

--- src/components/pages/Dasboard/index.js
+++ src/components/pages/Dasboard/index.js
@@ -1,32 +1,54 @@
 import React from 'react'
 import { Grid, GridItem } from '@chakra-ui/react'
+import Pomodoro from '../../Pomodoro'
+import GoogleSearch from '../../GoogleSearch'
 
 export default function Dashboard() {
   return (
     <div className='dashboard'>
+
+{/* <Grid
+  templateRows='repeat(4, 1fr)'
+  templateColumns='repeat(5, 1fr)'
+  gap={4}
+>
+  <GridItem rowSpan={1} colSpan={1} bg='tomato'> <Pomodoro/></GridItem>
+  <GridItem colSpan={2} bg='papayawhip'> <GoogleSearch/> </GridItem>
+  <GridItem colSpan={2} bg='papayawhip' />
+  <GridItem rowSpan={4} bg='papayawhip' />
+  <GridItem colSpan={4}  bg='tomato' />
+</Grid> */}
+
+
       <Grid
-  templateAreas={`"clock search"
+  templateAreas={`"clock quote"
+                  "side search"
                   "side main"
                   "side main"`}
-  gridTemplateRows={'10% 1fr 50px'}
-  gridTemplateColumns={'25% 1fr'}
+  gridTemplateRows={'.25fr .25fr .5fr 1.5fr'}
+  gridTemplateColumns={'.6fr 1.5fr'}
   h='93vh'
   gap='1'
   color='blackAlpha.700'
   fontWeight='bold'
 >
   <GridItem pl='2' bg='orange.300' area={'search'}>
-    Google Search
+    
   </GridItem>
+  <GridItem pl='2' bg='blue.300' area={'quote'}>
+    
+    </GridItem>
   <GridItem pl='2' bg='pink.300' area={'side'}>
     side
   </GridItem>
   <GridItem pl='2' bg='green.300' area={'main'}>
     Main
   </GridItem>
-  <GridItem pl='2' bg='blue.300' area={'clock'}>
-    Clock
+
+  <GridItem style={{justifyContent:"center",wordBreak:"break-all"}} pl='2' bg='blue.300' area={'clock'}>
+  <Pomodoro/>
   </GridItem>
+
   </Grid>
     </div>
   )
