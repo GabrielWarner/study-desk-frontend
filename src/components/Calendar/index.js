3ee1fe65f34953e1d8202d0bc395ece236a35324
Jiajie <jiajiehe@yahoo.com>
2022-09-01 16:56:39 -0700

    added a fetch request to grab the data from the database and render it to the page

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import DatePicker from "react-datepicker";
@@ -11,14 +11,14 @@ import "./index.css";
 
 const customStyles = {
     content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
     },
-  };
+};
 //   Modal.setAppElement('main');
 
 const DnDCalendar = withDragAndDrop(Calendar);
@@ -26,67 +26,50 @@ const DnDCalendar = withDragAndDrop(Calendar);
 const localizer = momentLocalizer(moment);
 
 
-const events = [
-    // {
-    //     title: "Project",
-    //     start: new Date(),
-    //     end: new Date("2022, 9, 5"),
-    // },
-    // {
-    //     title: "Metting",
-    //     start: new Date(),
-    //     end: new Date(),
-    // },
-    // {
-    //     title: "Class",
-    //     start: new Date("2022, 8, 28"),
-    //     end: new Date("2022, 8, 31"),
-    // },
-];
 
 
 function App() {
-    
+
     // Modal
     let subtitle;
     const [modalIsOpen, setIsOpen] = React.useState(false);
-  
+
     function openModal() {
-      setIsOpen(true);
+        setIsOpen(true);
     }
-  
+
     function afterOpenModal() {
-      subtitle.style.color = '#000000';
+        subtitle.style.color = '#000000';
     }
-  
+
     function closeModal() {
-      setIsOpen(false);
+        setIsOpen(false);
     }
 
     // Calendar 
     const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
-    const [allEvents, setAllEvents] = useState(events);
+    const [allEvents, setAllEvents] = useState([]);
 
     // Add Calendar Event to DB
     function handleAddEvent() {
-        fetch("http://localhost:3001/api/events",{
-            method:"POST",
-            body:JSON.stringify({
+        fetch("http://localhost:3001/api/events", {
+            method: "POST",
+            body: JSON.stringify({
                 ...newEvent
             }),
-            headers:{
-              "Content-Type":"application/json"
+            headers: {
+                "Content-Type": "application/json"
             }
-           }).then(res=>{
-              return res.json()
-           }).then(data=>{
+        }).then(res => {
+            return res.json()
+        }).then(data => {
             console.log(data)
             setNewEvent({
-              title:newEvent.title,
-              start:newEvent.start,
-              end:newEvent.end
+                title: newEvent.title,
+                start: newEvent.start,
+                end: newEvent.end
             })
-           })
+        })
 
 
 
@@ -94,7 +77,27 @@ function App() {
 
     }
 
-    // Fetch DB and Render to page
+    // // Fetch DB and Render to page
+    // // onMount - before
+    // // useEffect - callback
+    // useEffect(() => {
+    //     // on page load
+    //     // fetch the backend
+    //     fetch("http://localhost:3001/api/events", {
+    //         // method:"GET", default get route unleast specify
+    //         headers: {
+    //             "Content-Type": "application/json"
+    //         }
+
+    //     })
+    //         .then(res => {
+    //             return res.json()
+    //         })
+    //         .then(data => {
+    //             console.log(data)
+    //             setAllEvents(data)
+    //         })
+    // })
 
 
     return (
@@ -105,39 +108,39 @@ function App() {
 
             <div>
                 <button onClick={openModal}>Add Event</button>
-                    <Modal
-                        ariaHideApp={false}
-                        isOpen={modalIsOpen}
-                        onAfterOpen={afterOpenModal}
-                        onRequestClose={closeModal}
-                        style={customStyles}
-                        contentLabel="Calendar Modal"
-                    >
-                        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Add New Event</h2>
-                        <button onClick={closeModal}>Close</button>
-                        
-
-                            <div>
-                                <input type="text" placeholder="Add Title" value={newEvent.title} 
-                                onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
-                                <DatePicker placeholderText="Start Date" selected={newEvent.start} 
-                                onChange={(start) => setNewEvent({ ...newEvent, start })} />
-                                <DatePicker placeholderText="End Date" selected={newEvent.end} 
-                                onChange={(end) => setNewEvent({ ...newEvent, end })} />
-                                <button onClick={handleAddEvent}>
-                                    Add Event
-                                </button>
-                            </div>
+                <Modal
+                    ariaHideApp={false}
+                    isOpen={modalIsOpen}
+                    onAfterOpen={afterOpenModal}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    contentLabel="Calendar Modal"
+                >
+                    <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Add New Event</h2>
+                    <button onClick={closeModal}>Close</button>
 
 
-                    </Modal>
+                    <div>
+                        <input type="text" placeholder="Add Title" value={newEvent.title}
+                            onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })} />
+                        <DatePicker placeholderText="Start Date" selected={newEvent.start}
+                            onChange={(start) => setNewEvent({ ...newEvent, start })} />
+                        <DatePicker placeholderText="End Date" selected={newEvent.end}
+                            onChange={(end) => setNewEvent({ ...newEvent, end })} />
+                        <button onClick={handleAddEvent}>
+                            Add Event
+                        </button>
+                    </div>
+
+
+                </Modal>
             </div>
 
 
-            <DnDCalendar 
-                localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: "80vh", margin: "20px"}} 
+            <DnDCalendar
+                localizer={localizer} events={allEvents} startAccessor="start" endAccessor="end" style={{ height: "80vh", margin: "20px" }}
             />
-        
+
 
         </div>
     );
