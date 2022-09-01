13063518768b5626a97424b425adfa03395112cd
zmzhong1 <zhongzm8e@gmail.com>
2022-09-01 13:58:59 -0700

    update

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,39 +1,38 @@
-import React from 'react'
-import './style.css';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React from "react";
+import "./style.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 export default function NavigationBar({ user, handlePageChange, setUser }) {
   const logout = () => {
-    localStorage.removeItem("token")
-    setUser("")
-    handlePageChange('Home')
-  }
+    localStorage.removeItem("token");
+    setUser("");
+    handlePageChange("Home");
+  };
   return (
     <div>
-    <Navbar bg="dark" expand="lg">
-      <Container style={{ minHeight:"0" }}>
-        {/* Need to use conditional rendering to render either LOGOUT or HOME depending on if they are signed in or not */}
-          
-            {/* We might not need a homebutton since there is no point in going to homepage once you are logged in */}
-            <button onClick={() => handlePageChange('Home')}>HOME</button>
-            
+      <Navbar className="navColor" expand="lg">
+        <Container style={{ minHeight: "0" }}>
+          {/* Need to use conditional rendering to render either LOGOUT or HOME depending on if they are signed in or not */}
 
-            {/* onClick logout user and send them to home page */}
-            {user.email? 
+          {/* We might not need a homebutton since there is no point in going to homepage once you are logged in */}
+          <button onClick={() => handlePageChange("Home")}>HOME</button>
+
+          {/* onClick logout user and send them to home page */}
+          {user.email ? (
             <>
-            <button onClick={() => handlePageChange('Dashboard')}>DASHBOARD</button>
-            <button onClick={() => logout()}>LOGOUT</button>
+              <button onClick={() => handlePageChange("Dashboard")}>
+                DASHBOARD
+              </button>
+              <button onClick={() => logout()}>LOGOUT</button>
             </>
-              :(
-              <>
-              <button onClick={() => handlePageChange('Login')}>LOGIN</button>
-              </>
-            )}
-            
-
-      </Container>
-    </Navbar>
+          ) : (
+            <>
+              <button onClick={() => handlePageChange("Login")}>LOGIN</button>
+            </>
+          )}
+        </Container>
+      </Navbar>
     </div>
-  )
+  );
 }
--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
-export default function index({handlePageChange,user,setCurrentPage,setUser,setToken}) {
-  
+export default function index({
+  handlePageChange,
+  user,
+  setCurrentPage,
+  setUser,
+  setToken,
+}) {
   return (
-    <div>
-        <h1>Home Page</h1>
-        <p> Welcome to our application</p>
-        {user.id? setCurrentPage('Dashboard'):(<h1>hi</h1>)}
+    <div className="homeContainer">
+      <h1>Home Page</h1>
+      <p> Welcome to our application</p>
+      {user.id ? setCurrentPage("Dashboard") : <h1>hi</h1>}
     </div>
-    
-  )
+  );
 }
