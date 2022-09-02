672550b2a25526ca69549a4caa26fa292665fb38
Jiajie <jiajiehe@yahoo.com>
2022-09-02 16:08:16 -0700

    added calendar to dashboard

--- src/components/pages/Dasboard/index.js
+++ src/components/pages/Dasboard/index.js
@@ -6,7 +6,7 @@ import Inspirational from '../../Inspirational'
 import Weather from '../../Weather'
 import Notes from '../../Notes'
 import "./style.css";
-import { Calendar } from 'react-big-calendar'
+import Calendar from '../../Calendar'
 
 export default function Dashboard({setUser, setToken}) {
   useEffect(() => {
@@ -55,6 +55,7 @@ export default function Dashboard({setUser, setToken}) {
   <div id='search' className='search-bar'><GoogleSearch/></div>
   <div id='quote' className='quote-widget'><Inspirational/></div>
   <div id='main' className='notes-main'><Notes/></div>
+  <div id='calendar' className='calendar'><Calendar/></div>
 </div>
 
       {/* <Grid
