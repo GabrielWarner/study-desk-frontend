8a8c260c6862b5aa0b3f4a8894855fb8eb528638
GabrielWarner <gabe0412@uw.edu>
2022-09-04 16:03:30 -0700

    styling changes

--- src/components/pages/Dasboard/index.js
+++ src/components/pages/Dasboard/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
 import Pomodoro from "../../Pomodoro";
 import GoogleSearch from "../../GoogleSearch";
@@ -6,10 +6,19 @@ import Inspirational from "../../Inspirational";
 import Weather from "../../Weather";
 import Notes from "../../Notes";
 import Calendar from "../../Calendar";
-
+import Modal from "react-bootstrap/Modal";
 import "./style.css";
 
-export default function Dashboard({ setUser, setToken,setTimerToggle }) {
+export default function Dashboard({ setUser, setToken, setTimerToggle }) {
+  const [show, setShow] = useState(false);
+
+  const [timer, setTimer] = useState(true);
+  const [search, setSearch] = useState(true);
+  const [side, setSide] = useState(true);
+  const [notes, setNotes] = useState(true);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
@@ -39,22 +48,109 @@ export default function Dashboard({ setUser, setToken,setTimerToggle }) {
     <div className="dashboard">
       <div className="grid-container">
         <div className="lofiBackground"></div>
-      <div id="timer" className="timer">
-          <Pomodoro />
-      </div>
-
-        <div id="side" className="side">
-          <Weather />
-        </div>
-        <div id="search" className="search-bar">
-          <GoogleSearch />
-        </div>
         <div id="quote" className="quote-widget">
+          <button className="homeButton gadgetSetting" onClick={handleShow}>
+            GADGET SETTINGS
+          </button>
+          <Modal className="modalBorder" show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title className="modalTitle">Settings</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <input
+                onClick={() => {
+                  setTimer(!timer);
+                }}
+                id="timer"
+                type="checkbox"
+              ></input>
+              <label for="timer"> Pomodoro</label>
+              <br />
+
+              <input
+                onClick={() => {
+                  setSearch(!search);
+                }}
+                id="search"
+                type="checkbox"
+              ></input>
+              <label for="search"> Search</label>
+              <br />
+
+              <input
+                onClick={() => {
+                  setSide(!side);
+                }}
+                id="side"
+                type="checkbox"
+              ></input>
+              <label for="side"> Side</label>
+              <br />
+
+              <input
+                onClick={() => {
+                  setNotes(!notes);
+                }}
+                id="notes"
+                type="checkbox"
+              ></input>
+              <label for="notes"> Notes</label>
+              <br />
+            </Modal.Body>
+            <Modal.Footer>
+              <button className="homeButton" onClick={handleClose}>
+                Close
+              </button>
+              <button className="homeButton" onClick={handleClose}>
+                Save Changes
+              </button>
+            </Modal.Footer>
+          </Modal>
           <Inspirational />
         </div>
-        <div id="main" className="notes-main">
+
+        {timer ? (
+          <div id="timer" className="timer">
+            {/* <div className="textOpacity"> */}
+            <Pomodoro />
+            {/* </div> */}
+          </div>
+        ) : (
+          <div id="timer" className="hideTimer">
+          <Pomodoro />
+        </div>
+        )
+        }
+
+        {search ? (
+          <div id="search" className="search-bar">
+            <GoogleSearch />
+          </div>
+        ) : (
+          <div id="search" className="hideSearchBar">
+          <GoogleSearch />
+        </div>
+        )}
+
+        {side ? (
+          <div id="side" className="side">
+            <Weather />
+          </div>
+        ) : (
+          <div id="side" className="hideSide">
+          <Weather />
+        </div>
+        )}
+
+        {notes ? (
+          <div id="main" className="notes-main">
+            <Notes />
+          </div>
+        ) : (
+          <div id="main" className="hideNote">
           <Notes />
         </div>
+        )}
       </div>
     </div>
   );
